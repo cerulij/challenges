2b3eb1a1744a4cb692e58748a314d36b0a2fe38c
cerulij <Polik010290>
2021-09-01 11:46:07 -0400

    update

---
+++ src/String/ReverseInteger.java
@@ -0,0 +1,37 @@
+package String;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class ReverseInteger {
+
+  public int reverse(int x) {
+    int result = 0;
+
+    while (x != 0) {
+      int pop = x % 10;
+      x = x / 10;
+
+      // Integer.MAX_VALUE = 2147483647
+      // max / 10 = 214748364
+      // If result > max / 10, when adding pop it will overflow
+      // If result = max / 10, pop must be greater than 7 to overflow
+      if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && pop > 7)) {
+        return 0;
+      }
+
+      // Integer.MIN_VALUE = -2147483648
+      // min / 10 = -214748364
+      // If result < min / 10, when adding pop it will overflow
+      // If result - min / 10, pop must be less than -8
+      if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && pop < -8)) {
+        return 0;
+      }
+
+      result = result * 10 + pop;
+    }
+
+    return result;
+  }
+
+}
