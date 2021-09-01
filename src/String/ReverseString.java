9eac4db7eee54ac7633a2804d433b48ec72b8ec6
cerulij <Polik010290>
2021-09-01 10:45:56 -0400

    update

--- src/String/ReverseString.java
+++ src/String/ReverseString.java
@@ -6,8 +6,8 @@ public class ReverseString {
 
   public void reverseString(char[] s) {
     int n = s.length;
-
     int j = n - 1;
+
     for (int i = 0; i < n / 2; i++) {
       char temp = s[j];
       s[n - 1 - i] = s[i];
@@ -17,6 +17,19 @@ public class ReverseString {
 
     System.out.println(Arrays.toString(s));
   }
+
+
+  public void reverseStringWhile(char[] s) {
+    int left = 0;
+    int right = s.length - 1;
+
+    while (left < right) {
+      char temp = s[left];
+      s[left++] = s[right];
+      s[right--] = temp;
+    }
+  }
+
 
   public static void main (String[] args) {
     char[] s = {'h'};
