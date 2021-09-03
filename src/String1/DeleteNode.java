6ab643157121dea2445f8e3481880d92374e448d
cerulij <Polik010290>
2021-09-03 11:26:54 -0400

    update

---
+++ src/String1/DeleteNode.java
@@ -0,0 +1,46 @@
+// Delete a node without knowing the head of the singly linked list
+
+package String1;
+
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+  }
+}
+
+
+public class DeleteNode {
+
+  public void deleteNode(ListNode node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+  }
+
+
+  public static void main (String[] args) {
+    ListNode node1 = new ListNode(4);
+    ListNode node2 = new ListNode(5);
+    ListNode node3 = new ListNode(1);
+    ListNode node4 = new ListNode(9);
+
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+
+    ListNode iterator = node1;
+
+    new DeleteNode().deleteNode(node3);
+
+    while (iterator != null) {
+      System.out.println(iterator.val);
+      iterator = iterator.next;
+    }
+  }
+
+}
+
+
